b1ea4c2fc1570692e2a03a1d04521b8a7d114c53
Axel Rodriguez <alejandrom9712@gmail.com>
2019-11-11 01:53:13 -0600

    lambdaGenerator funcionando para generar codigo qr e insertar registro en la base de datos

--- lambdaGenerator/index.js
+++ lambdaGenerator/index.js
@@ -2,7 +2,7 @@ var aws = require('aws-sdk');
 var qr = require('qr-image');
 var moment = require('moment');
 
-const bucket = '';
+const bucket = 'qrCodes';
 const id = '';
 const secret = '';
 
@@ -10,7 +10,7 @@ exports.handler = function(event, context, callback) {
     var unixTime = moment().unix().toString();
     var fileName = unixTime + '.png';
     var code = qr.imageSync(unixTime, { type: 'png' });
-
+    var response;
     const s3Bucket = new aws.S3({
         credentials : {
             accessKeyId: id,
@@ -24,13 +24,30 @@ exports.handler = function(event, context, callback) {
     };
     s3Bucket.upload(params, function(err, data) {
         if (err) {
-            return callback({
-            statusCode: 500
-            }, null);
+            response = {
+                statusCode: 500,
+                error: err
+            };
         } else {
-            return callback({
-            statusCode: 200
-            }, null);
+            insertData(unixTime, fileName)
+                .then(() => {
+                    console.log('termino');
+                    response = {
+                        statusCode: 200
+                    };
+                });
         }
     });
+    return response;
+};
+
+var insertData = async (unT, fNa) => {
+    const { Client } = require('pg');
+    const client = new Client();
+    await client.connect();
+    var queryText = "INSERT INTO carwashschema.codigos (codeid, filename) VALUES ($1, $2)";
+    const res = await client.query(
+        queryText, 
+        [unT, fNa]);
+    await client.end();
 };
